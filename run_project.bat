02ef807ae94fc6982daf347f51645dac533a87a9
andsandoval <asandoval@plu.edu>
2025-03-07 20:12:21 -0800

    Updated script to open 2 separate cmds, allows for individual tracking of back-end and front-end

--- run_project.bat
+++ run_project.bat
@@ -1,11 +1,8 @@
-:: Script to run both the node.js backend and flutter frontend at the same time
-:: To run the script, run ./run_project.bat in the terminal
-
 @echo off
 echo Starting back-end and front-end...
 
-REM Start back-end in the background
-start /B npm start --prefix backend
+REM Start back-end in a new Command Prompt window
+start "Back-End" cmd /k "cd backend && npm start"
 
-REM Start front-end in the foreground
-flutter run lib/main.dart
+REM Start front-end in a new Command Prompt window
+start "Front-End" cmd /k "flutter run"
