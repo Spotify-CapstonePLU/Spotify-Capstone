bb8b1415c62ed2071771dc996420449dea769184
andsandoval <asandoval@plu.edu>
2025-03-11 07:10:18 -0700

    Updated scripts that run front-end and back-end together to run front-end on port 8000

--- run_project.bat
+++ run_project.bat
@@ -5,4 +5,4 @@ REM Start back-end in a new Command Prompt window
 start "Back-End" cmd /k "cd backend && npm start"
 
 REM Start front-end in a new Command Prompt window
-start "Front-End" cmd /k "flutter run"
+start "Front-End" cmd /k "flutter run --web-port=8000"
