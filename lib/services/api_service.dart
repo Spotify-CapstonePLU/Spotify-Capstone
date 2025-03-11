64a31f8aba66cd2fd6354afdf444252fae83b9ef
andsandoval <asandoval@plu.edu>
2025-03-11 07:08:10 -0700

    Implement Spotify authorization URL launch in ApiService

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,10 +1,17 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'dart:developer';
+import 'package:url_launcher/url_launcher.dart';
 
 class ApiService {
   static const String baseUrl = "http://localhost:3000";
 
   static Future<dynamic> getSpotifyAuthorization() async {
-    final response = await http.get(Uri.parse('$baseUrl/auth'));
+    const authUrl = 'http://localhost:3000/auth';
+    if (await canLaunchUrl(Uri.parse(authUrl))) {
+      await launchUrl(Uri.parse(authUrl));
+    } else {
+      log("Could not launch login");
+    }
   }
 }
