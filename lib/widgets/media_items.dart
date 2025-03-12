6e11c87fe205494517ba8946546c9ff7509d062f
Eric Sun <eric.sun@plu.edu>
2025-03-11 21:23:37 -0700

    Removed MediaItem class.

--- lib/widgets/media_items.dart
+++ lib/widgets/media_items.dart
@@ -32,55 +32,6 @@ class _MediaItemListState extends State<MediaItemList> {
   }
 }
 
-class MediaItem extends StatelessWidget {
-  const MediaItem({super.key, this.itemData, this.onTap});
-  final VoidCallback? onTap;
-  final MediaItemData? itemData;
-
-  @override
-  Widget build(BuildContext context) {
-    double containerHeight = MediaQuery.sizeOf(context).height;
-
-    double itemMargin = containerHeight * 0.01;
-    double itemPadding = containerHeight * 0.01;
-    double imageSizeConstraint = containerHeight * 0.09;
-    return GestureDetector(
-      onTap: () {
-        if (onTap != null) {
-          onTap!();
-        }
-      },
-      child: Container(
-        margin: EdgeInsets.fromLTRB(itemMargin, 0, itemMargin, itemMargin),
-        padding: EdgeInsets.all(itemPadding),
-        constraints: BoxConstraints(maxHeight: containerHeight * 0.12),
-        decoration: const BoxDecoration(color: Colors.blue),
-        child:
-            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-          Row(
-            children: [
-              Container(
-                  constraints: BoxConstraints(
-                      maxHeight: imageSizeConstraint,
-                      maxWidth: imageSizeConstraint),
-                  margin: EdgeInsets.fromLTRB(0, 0, containerHeight * 0.05, 0),
-                  child: Image.network(itemData!.imageUrl)),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(itemData!.title),
-                  Text(itemData!.details),
-                ],
-              )
-            ],
-          ),
-          IconButton(onPressed: () => {}, icon: const Icon(Icons.more_vert))
-        ]),
-      ),
-    );
-  }
-}
 
 class MediaItemData {
   const MediaItemData({
