1adf060d86cb3901700fe1ea3db3f375e2bbbbf0
andsandoval <asandoval@plu.edu>
2025-03-11 19:01:41 -0700

    Update API service URLs to use 127.0.0.1 - Fixes a bug that saves and reads cookies from localhost instead of 127.0.0.1, causing issues in finding cookies

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -4,10 +4,10 @@ import 'dart:developer';
 import 'package:url_launcher/url_launcher.dart';
 
 class ApiService {
-  static const String baseUrl = "http://localhost:3000";
+  static const String baseUrl = "http://127.0.0.1:3000";
 
   static Future<dynamic> getSpotifyAuthorization() async {
-    const authUrl = 'http://localhost:3000/auth';
+    const authUrl = 'http://127.0.0.1:3000/auth';
     if (await canLaunchUrl(Uri.parse(authUrl))) {
       await launchUrl(Uri.parse(authUrl));
     } else {
