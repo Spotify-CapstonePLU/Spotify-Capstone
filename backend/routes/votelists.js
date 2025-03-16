099f760e0a92fcc93d72730c66f10d68ad5cab74
Eric Sun <eric.sun@plu.edu>
2025-03-15 22:16:25 -0700

    Added route for getting all of user's votelists from out database.

--- backend/routes/votelists.js
+++ backend/routes/votelists.js
@@ -4,5 +4,22 @@ import { Pool } from 'pg';
 const router = Router();
 const pool = new Pool({ connectionString: process.env.DB_HOST, ssl: { rejectUnauthorized: false } });
 
+// Get all user's votelists
+router.get('/votelists', async (req, res) => {
+    // TODO VerifyTokens
+    // TODO get user id from spotify
+    const { id } = req.params;
+    try {
+        const result = await pool.query(`SELECT Votelist.* FROM Votelist
+                                         JOIN Collaborators ON Collaborators.playlist_id = Votelist.playlist_id
+                                         WHERE Collaborators.user_id = $1`,
+            [id]
+        );
+        res.json(result.rows);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Server error');
+    }
+});
 
 export default router;
