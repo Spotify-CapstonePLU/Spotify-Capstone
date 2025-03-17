3ab8ee40e59c7eb6d00c2e0a024354034ee24a83
Eric Sun <eric.sun@plu.edu>
2025-03-17 03:51:09 -0700

    Added endpoint in voting.js for getting polls based on playlist/votelist id.

--- backend/routes/voting.js
+++ backend/routes/voting.js
@@ -6,5 +6,20 @@ const axios = new Axios();
 const router = Router();
 const pool = new Pool({ connectionString: process.env.DB_HOST, ssl: { rejectUnauthorized: false } });
 
+// Get all polls for votelist
+router.get('/voting/:playlist_id', async (req, res) => {
+    // TODO VerifyTokens
+    const { playlist_id } = req.params;
+    try {
+        const result = await pool.query(`SELECT * from Poll
+                                         WHERE playlist_id = $1`,
+            [playlist_id]
+        );
+        res.json(result.rows);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Server error');
+    }
+});
 
 export default router;
