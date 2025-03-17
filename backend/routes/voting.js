8a2fc670f98eeade58237a34ff1ac83eab3ca530
Eric Sun <eric.sun@plu.edu>
2025-03-17 03:46:42 -0700

    Added axios to voting.js for calling to Spotify API.

--- backend/routes/voting.js
+++ backend/routes/voting.js
@@ -1,6 +1,8 @@
 import { Router } from 'express';
 import { Pool } from 'pg';
+import { Axios } from 'axios';
 
+const axios = new Axios();
 const router = Router();
 const pool = new Pool({ connectionString: process.env.DB_HOST, ssl: { rejectUnauthorized: false } });
 
