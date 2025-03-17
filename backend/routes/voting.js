be7a8481e3782b79af84d1907d1e1316229f752a
Eric Sun <eric.sun@plu.edu>
2025-03-17 03:55:26 -0700

    Added endpoint in voting.js for updating poll votes.

--- backend/routes/voting.js
+++ backend/routes/voting.js
@@ -22,4 +22,28 @@ router.get('/voting/:playlist_id', async (req, res) => {
     }
 });
 
+// Update a poll's votes
+router.post('/voting', async (req, res) => {
+    // TODO VerifyTokens
+    const { poll_id, vote } = req.body;
+    try {
+        const result = vote ? await pool.query(
+            `UPDATE Poll SET upvotes = upvotes + 1
+             WHERE poll_id = $1
+             RETURNING *`,
+            [poll_id]
+        ) :
+        await pool.query(
+            `UPDATE Poll SET downvotes = downvotes + 1
+             WHERE poll_id = $1
+             RETURNING *`,
+            [poll_id]
+        );
+        res.json(result.rows[0]);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error updating poll.');
+    }
+});
+
 export default router;
