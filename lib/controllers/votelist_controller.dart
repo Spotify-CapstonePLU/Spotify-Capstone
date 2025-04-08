2a6bbe6867b409502b99addc24ec43606eb4771f
andsandov <asandoval@plu.edu>
2025-04-07 21:06:30 -0700

    Updated VotelistController with registerVotelist and getUserPlaylists methods; added some error handling for API requests

--- lib/controllers/votelist_controller.dart
+++ lib/controllers/votelist_controller.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'dart:developer';
 import 'dart:html';
 
+import 'package:spotify_polls/models/media_item.dart';
 import 'package:spotify_polls/models/votelist.dart';
 
 class VotelistController {
@@ -46,6 +47,38 @@ class VotelistController {
     } else {
       throw Exception(
           'Failed to create Votelist with status: ${request.status}');
+    }
+  }
+
+  Future<Votelist> registerVotelist(String id, String name) async {
+    final request = await HttpRequest.request('$baseUrl/votelists/register',
+        method: 'POST',
+        requestHeaders: {'Content-Type': 'application/json'},
+        withCredentials: true,
+        sendData: jsonEncode({'playlist_id': id, 'playlist_name': name}));
+
+    if (request.status == 201) {
+      return Votelist.fromJson(
+          jsonDecode(request.responseText!) as Map<String, dynamic>);
+    } else {
+      throw Exception(
+          'Failed to register the Votelist with status: ${request.status}');
+    }
+  }
+
+  Future<MediaItemData> getUserPlaylists() async {
+    final request = await HttpRequest.request(
+      '$baseUrl/votelists/playlists',
+      method: 'GET',
+      requestHeaders: {'Content-Type': 'application/json'},
+      withCredentials: true,
+    );
+
+    if (request.status == 200) {
+      return const MediaItemData(title: 'title', details: 'details');
+    } else {
+      throw Exception(
+          'Failed to retrieve the user\'s playlists with status: ${request.status}');
     }
   }
 }
