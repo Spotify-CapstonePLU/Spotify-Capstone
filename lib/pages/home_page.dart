e54c1c0c3af74e6a4e55f07fd74c9fa98f2e5730
Vincent Sison <vincentallenarceosison@gmail.com>
2025-04-26 13:11:27 -0700

    Implement ui_styles to home_page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -5,6 +5,7 @@ import 'package:spotify_polls/pages/live_login_page.dart';
 import '../assets/constants.dart' as constants;
 import 'package:spotify_polls/services/api_service.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:spotify_polls/styles/ui_styles.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key, this.title = "Home Page"});
@@ -19,12 +20,13 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: UiStyles.backgroundColor,
       body: SingleChildScrollView(
         child: Container(
           constraints: BoxConstraints(
             minHeight: MediaQuery.of(context).size.height,
           ),
-          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 40),
+          padding: UiStyles.screenPadding,
           child: const Column(
             mainAxisAlignment: MainAxisAlignment.center, // Distributes elements
             crossAxisAlignment: CrossAxisAlignment.center,
@@ -45,8 +47,8 @@ class TitleSection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(16.0),
+    return const Padding(
+      padding: EdgeInsets.all(16.0),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         mainAxisSize: MainAxisSize.min,
@@ -54,14 +56,14 @@ class TitleSection extends StatelessWidget {
           Flexible(
               child: Text(
             constants.appName,
-            style: Theme.of(context).textTheme.headlineLarge,
+            style: UiStyles.heading1,
             textAlign: TextAlign.center,
           )),
-          const SizedBox(height: 25,),
+          SizedBox(height: 25,),
           Flexible(
               child: Text(
             constants.appDescription,
-            style: Theme.of(context).textTheme.headlineMedium,
+            style: UiStyles.heading2,
             textAlign: TextAlign.center,
           ))
         ],
@@ -81,12 +83,14 @@ class ButtonSection extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
           TextButton(
+              style: UiStyles.textButtonStyle,
               onPressed: () {
                 log("clicked Sign In");
                 ApiService.getSpotifyAuthorization(context);
               },
               child: const Text(constants.signIn)),
           TextButton(
+              style: UiStyles.textButtonStyle,
               onPressed: () {
                 log("clicked Join Live");
                 Navigator.push(
