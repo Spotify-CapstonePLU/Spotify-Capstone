3fc987563ff5c04bafddd4ae90f504a9216aaaf1
Eric Sun <eric.sun@plu.edu>
2025-05-01 17:47:17 -0700

    Creation of playlist.dart model

---
+++ lib/models/playlist.dart
@@ -0,0 +1,24 @@
+import 'dart:ui';
+import 'package:spotify_polls/models/media_item.dart';
+
+class Playlist extends MediaItemData {
+  final String playlistId;
+
+  const Playlist({
+    required this.playlistId,
+    required super.title,
+    required super.details
+  });
+
+  factory Playlist.fromJson(Map<String, dynamic> json) {
+    return Playlist(
+        title: json['playlist_name'],
+        details: '',
+        playlistId: json['playlist_id'],);
+  }
+
+  @override
+  String toString() {
+    return '[Title: $title, Details: $details, ItemId: $playlistId]';
+  }
+}
