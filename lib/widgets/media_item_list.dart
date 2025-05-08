db0aaac15f6137de11c04e901c308ae3dfc24de0
Eric Sun <eric.sun@plu.edu>
2025-05-08 04:13:57 -0700

    fixed bug where items in media_item_list.dart weren't tappable.

--- lib/widgets/media_item_list.dart
+++ lib/widgets/media_item_list.dart
@@ -31,60 +31,63 @@ class _MediaItemListState extends State<MediaItemList> {
           border: Border.all(),
         ),
         child: ClipRRect(
-          borderRadius: BorderRadius.circular(16),
-          child: Padding(
-            padding: const EdgeInsets.fromLTRB(8, 8, 8, 8),
-            child: ListView.separated(
-              shrinkWrap: true,
-              itemBuilder: (_, index) {
-                var curData = widget.listData[index];
-                return Material(
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.amber,
+            borderRadius: BorderRadius.circular(16),
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(8, 8, 8, 8),
+              child: ListView.separated(
+                shrinkWrap: true,
+                itemBuilder: (_, index) {
+                  var curData = widget.listData[index];
+                  return Material(
+                    child: InkWell(
+                      onTap: curData.onTap,
                       borderRadius: BorderRadius.circular(16),
-                    ),
-                    padding: const EdgeInsets.all(8),
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.start, // Allows image to be taller
-                      children: [
-                        SizedBox(
-                          width: 75,
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(12),
-                            child: Image.network(
-                              curData.imageUrl,
-                              fit: BoxFit.cover,
-                              errorBuilder: (context, error, stackTrace) {
-                                return Image.network('lib/assets/trackArtPlaceholder.png');
-                              },
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.amber,
+                          borderRadius: BorderRadius.circular(16),
+                        ),
+                        padding: const EdgeInsets.all(8),
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment
+                            .start, // Allows image to be taller
+                          children: [
+                            SizedBox(
+                              width: 75,
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(12),
+                                child: Image.network(
+                                  curData.imageUrl,
+                                  fit: BoxFit.cover,
+                                  errorBuilder: (context, error, stackTrace) {
+                                    return Image.network(
+                                        'lib/assets/trackArtPlaceholder.png');
+                                  },
+                                ),
+                              ),
                             ),
-                          ),
+                            const SizedBox(width: 10),
+                            Expanded(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(curData.title),
+                                  const SizedBox(height: 4),
+                                  Text(curData.details),
+                                ],
+                              ),
+                            ),
+                          ],
                         ),
-                        const SizedBox(width: 10),
-                        Expanded(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(curData.title),
-                              const SizedBox(height: 4),
-                              Text(curData.details),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                );
-
-              },
-              separatorBuilder: (_, __) => const SizedBox(
-                height: 10,
+                      ),
+                    )
+                  );
+                },
+                separatorBuilder: (_, __) => const SizedBox(
+                  height: 10,
+                ),
+                itemCount: widget.listData.length,
               ),
-              itemCount: widget.listData.length,
-            ),
-          )
-        )
-    );
+            )));
   }
 }
